3c1dc70eda1e376c3e9c19fc818fbbd06907b407
MarePri <marko.cena@gmail.com>
2026-06-03 08:40:00 +0200

    Add my new file

--- Desktop/cypressprjct/cypress/e2e/homePageTest.cy.js
+++ Desktop/cypressprjct/cypress/e2e/homePageTest.cy.js
@@ -1,44 +1,31 @@
 /// <reference types="cypress"/>
 
-describe('Provera Kontakt Informacija na Početnoj Stranici', () => {
-    beforeEach(() => {
-      // Pretpostavljamo da ste navigirali do početne stranice ili je posetili
-      cy.visit('https://www.games.rs'); // Zamijenite sa stvarnom URL adresom početne stranice
+describe("Provera HP", () => {
+  beforeEach(() => {
+    cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
+    cy.visit("https://www.games.rs");
+  });
+
+  it("treba da sadrži telefon i e-mail", () => {
+    cy.get(".item-telephone").should("exist");
+  });
+  it("treba da sadrži linkove", () => {
+    cy.contains("a", "KONTAKT")
+      .should("have.attr", "href")
+      .and("include", "/kontakt");
+
+    cy.contains("a", "O NAMA")
+      .should("have.attr", "href")
+      .and("include", "/o-nama");
+
+    cy.contains("a", "POMOĆ")
+      .should("have.attr", "href")
+      .and("include", "/najcesca-pitanja");
+  });
+
+  it("provera logo", () => {
+    it("treba da se vidi logo", () => {
+      cy.get(".block logo").should("be.visible");
     });
-    
-    it('treba da sadrži telefon i e-mail', () => {
-      // Provera postojanja telefonskog broja
-      cy.get('.item-telephone') // Zamijenite sa stvarnom klasom ili identifikatorom za telefon
-        .should('exist');
-      // Provera postojanja e-mail adrese
-    })
-        it('treba da sadrži linkove "Kontakt", "O nama" i "Pomoć" sa ispravnim URL-ovima', () => {
-          // Provera postojanja linka "Kontakt" i ispravnosti URL-a
-          cy.contains('a', 'KONTAKT')
-            .should('have.attr', 'href')
-            .and('include', '/kontakt'); // Zamijenite '/kontakt' sa stvarnim delom URL-a za stranicu "Kontakt"
-      
-          // Provera postojanja linka "O nama" i ispravnosti URL-a
-          cy.contains('a', 'O NAMA')
-            .should('have.attr', 'href')
-            .and('include', '/o-nama'); // Zamijenite '/o-nama' sa stvarnim delom URL-a za stranicu "O nama"
-      
-          // Provera postojanja linka "Pomoć" i ispravnosti URL-a
-          cy.contains('a', 'POMOĆ')
-            .should('have.attr', 'href')
-            .and('include', '/najcesca-pitanja'); // Zamijenite '/pomoc' sa stvarnim delom URL-a za stranicu "Pomoć"
-        });
-        
-        it('Provera Vidljivosti Logoa i Slika', () => {
-        
-          it('treba da sadrži vidljiv logo', () => {
-            // Provera vidljivosti logoa
-            cy.get('.block logo') // Zamijenite sa stvarnom klasom ili identifikatorom logoa
-              .should('be.visible');
-          });
-        });
-        
-      
-    });
- 
-  
+  });
+});
--- Desktop/cypressprjct/cypress/e2e/staticPageTest.cy.js
+++ Desktop/cypressprjct/cypress/e2e/staticPageTest.cy.js
@@ -1,28 +1,37 @@
-describe('Provera Statickih Stranica', () => {
-    const staticPages = [
-      { url: '/o-nama', title: 'O nama', contentString: 'O našoj kompaniji' },
-      { url: '/uslovi-koriscenja', title: 'Uslovi korišćenja i prodaje', contentString: 'Opšti uslovi' }
-      // Dodajte druge stranice prema potrebi
-    ];
-  
-    beforeEach(() => {
-      // Pretpostavljamo da ste navigirali do početne stranice ili je posetili
-      cy.visit('https://www.games.rs'); // Zamijenite sa stvarnom URL adresom početne stranice
-    });
-  
-    it('treba da ima tačne URL-ove, naslove i stringove na statičkim stranicama', () => {
-      staticPages.forEach((page) => {
-        cy.visit(page.url);
-  
-        // Provera tačnosti URL-a
-        cy.url().should('include', page.url);
-  
-        // Provera prisutnosti naslova
-        cy.get('h1').should('contain', page.title);
-  
-        // Provera prisutnosti određenog stringa na stranici
-        cy.get('body').should('contain', page.contentString);
+describe("Provera Static page", () => {
+  const staticPages = [
+    {
+      url: "/o-nama",
+      title: "O nama",
+      contentString:
+        "GameS je lanac prodavnica specijalizovan za prodaju konzola",
+    },
+  ];
+
+  beforeEach(() => {
+    cy.visit("https://www.games.rs");
+  });
+
+  it("Static page", () => {
+    staticPages.forEach((page) => {
+      cy.visit("https://www.games.rs/o-nama");
+
+      cy.url().should("include", page.url);
+
+      cy.get("h1").should("contain", page.title);
+
+      cy.get("body").should("contain", page.contentString);
+
+      it("tacan url i h1", () => {
+        cy.contains("a", "Uslovi korišćenja i prodaje").click();
+
+        cy.url().should("include", "/uslovi-koriscenja");
+
+        cy.get("h1").should("contain", "Uslovi korišćenja i prodaje");
+
+        cy.get("body").should("contain", "Opšti uslovi");
+        cy.get("body").should("contain", "Prodaja i isporuka");
       });
     });
   });
-  
+});

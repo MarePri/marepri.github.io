e04162459b2f84070d16e101a5b990f16240f64c
Marko Prihosko <marko@example.com>
2026-06-30 10:11:12 +0000

    Fix portfolio: remove stray files, drop broken flashing-text animation, add viewport/meta tags

--- script.js
+++ script.js
@@ -2,13 +2,15 @@
 var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
 
-var button = document.getElementById('hover');
+var buttons = document.querySelectorAll('.bubble-link');
 var body = document.body;
 
-button.onmouseover = function() {
-    body.className = 'hovered';
-}
+buttons.forEach(function(button) {
+    button.onmouseover = function() {
+        body.className = 'hovered';
+    }
 
-button.onmouseout = function() {
-    body.className = '';
-}
+    button.onmouseout = function() {
+        body.className = '';
+    }
+});
